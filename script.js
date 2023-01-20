f1fd8997f7bc9dfb2ba045c8b357eb233f0ea933
ManiakJ <117768385+ManiakJ@users.noreply.github.com>
2023-01-20 16:56:39 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+function f1(foto) {
+    if (foto == 'z1')
+    document.getElementById('glownezdjecie').src = 'https://cdn.discordapp.com/attachments/670725437143384104/1012850768765779989/301084708_3224584987795200_2756321613834456387_n.png'
+
+    if (foto == 'z2')
+    document.getElementById('glownezdjecie').src = 'Jakub Janowski /PedoKuba.png'
+
+    if (foto == 'z3')
+    document.getElementById('glownezdjecie').src = 'Jakub Janowski /ViLoveMielnoMelka.png'
+
+    if (foto == 'z4')
+    document.getElementById('glownezdjecie').src = 'Jakub Janowski /Njwiększa Fanka kubatoja456.png'
+
+    if (foto == 'z5')
+    document.getElementById('glownezdjecie').src = 'SS/ss2.png'
+
+    if (foto == 'z6')
+    document.getElementById('glownezdjecie').src = 'SS/ss3.png'
+
+    if (foto == 'z7')
+    document.getElementById('glownezdjecie').src = 'SS/ss4.png'
+
+    if (foto == 'z8')
+    document.getElementById('glownezdjecie').src = "SS/ss6.png"
+}
+
+function playSound() {
+    document.getElementById('michaloh').play();
+  };
